198effd0c38a9df46d984e727b68041a33349083
Uberdar <allexvik@gmail.com>
2020-05-28 18:39:18 +0300

    v 0.1

---
+++ js/index.js
@@ -0,0 +1,57 @@
+var a = document.getElementById("js_burger_btn");
+var s = document.getElementById("js_menu_flow");
+
+var burgerBtn = document.getElementById("js_burger_btn");
+burgerBtn.addEventListener("click", function (e) {
+  e.stopPropagation(); /*уточнить на счет этой функции*/
+  magicHappens();
+});
+function magicHappens() {
+  a.classList.toggle("burger_rotation");
+  s.classList.toggle("flowing_navigation");
+}
+
+document.addEventListener("click", function (e) {
+  a.classList.remove("burger_rotation");
+  s.classList.remove("flowing_navigation");
+});
+
+
+$(document).ready(function () {
+    $(".diet__slider").slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      dots: true,
+      autoplay: true,
+      autoplaySpeed: 1000,
+      responsive: [
+        {
+          breakpoint: 575,
+          settings: {
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: false,
+            autoplay: true,
+            autoplaySpeed: 1000,
+          },
+        },
+      ],
+    });
+  });
+
+
+  $(document).ready(function () {
+    $(".slider__prof-mob").slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      dots: false,
+      autoplay: true,
+      autoplaySpeed: 1000,
+    });
+  });
